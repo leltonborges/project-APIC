87ea1bfe3b90bcbe0ff4adcb08fe840e9465dd51
Lelton Borges <leltondev@gmail.com>
2022-06-29 19:37:30 -0300

    feat(IconComponent): created component

--- project_pic/src/app/common/common-custom.module.ts
+++ project_pic/src/app/common/common-custom.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
+import { IconComponent } from './components/icon/icon.component';
 
 @NgModule({
   declarations: [
-    ButtonComponent
+    ButtonComponent,
+    IconComponent
   ],
   exports: [
     ButtonComponent,
--- project_pic/src/app/photo/photo.module.ts
+++ project_pic/src/app/photo/photo.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
