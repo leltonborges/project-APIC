798c6675f13eaa74c8003f59ef069331047ee298
Lelton Borges <leltondev@gmail.com>
2022-06-29 23:44:50 -0300

    feat(Search): created component

--- project_pic/src/app/common/common-custom.module.ts
+++ project_pic/src/app/common/common-custom.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
 import { IconComponent } from './components/icon/icon.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     ButtonComponent,
-    IconComponent
+    IconComponent,
+    SearchComponent
   ],
     exports: [
         ButtonComponent,
