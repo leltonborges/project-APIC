1adc4ee5c85c97e2a4e98a4bf7f61b97861b5e3a
Lelton Borges <leltondev@gmail.com>
2022-06-29 23:31:42 -0300

    feat(PhotoCard): created component

--- project_pic/src/app/photo/photo.module.ts
+++ project_pic/src/app/photo/photo.module.ts
@@ -8,6 +8,7 @@ import { PhotoAllComponent } from './photo-all/photo-all.component';
 import { FormsModule } from '@angular/forms';
 import { FilterByDescriptionPipe } from './photo-list/filter-by-description.pipe';
 import { CommonCustomModule } from '../common/common-custom.module';
+import { PhotoCardComponent } from './photo-card/photo-card.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { CommonCustomModule } from '../common/common-custom.module';
     PhotoListComponent,
     PhotoFormComponent,
     PhotoAllComponent,
-    FilterByDescriptionPipe
+    FilterByDescriptionPipe,
+    PhotoCardComponent
   ],
   exports: [
     PhotoListComponent,
