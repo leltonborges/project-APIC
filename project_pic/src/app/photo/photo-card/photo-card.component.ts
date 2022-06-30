a1cd257c6255ccd8f5040a8b0800bf107d14fcf9
Lelton Borges <leltondev@gmail.com>
2022-06-29 23:38:17 -0300

    chore(PhotoCard): Implements template and component

--- project_pic/src/app/photo/photo-card/photo-card.component.ts
+++ project_pic/src/app/photo/photo-card/photo-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Photo } from '../photo';
 
 @Component({
   selector: 'app-photo-card',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo-card.component.scss']
 })
 export class PhotoCardComponent implements OnInit {
+  private _photo!: Photo;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  get photo() : Photo {
+    return this._photo;
+  }
+
+  @Input('photo')
+  set photo(value : Photo) {
+    this._photo = value;
+  }
 }
