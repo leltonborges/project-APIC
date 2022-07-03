67ee69ce0f78d2f7a1e5407cc3bc76ffe832a29f
Lelton Borges <leltondev@gmail.com>
2022-07-02 22:09:04 -0300

    chore(TokenService): implements

--- project_pic/src/app/core/service/token/token.service.ts
+++ project_pic/src/app/core/service/token/token.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
 
+const KEY_TOKEN_API = 'man_access_value';
+
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
 
   constructor() { }
+
+  getToken() : string {
+    return localStorage.getItem(KEY_TOKEN_API) ?? '';
+  }
+
+  saveToken(token : string) : void {
+    localStorage.setItem(KEY_TOKEN_API, token);
+  }
+
+  deleteToken() : void {
+    localStorage.removeItem(KEY_TOKEN_API);
+  }
+
+  isToken() : boolean {
+    return !!this.getToken();
+  }
+
 }
