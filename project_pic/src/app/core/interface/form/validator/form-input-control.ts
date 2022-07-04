ff91a9204577fcb612f5e08f7cd4d15c66f8e6f1
Lelton Borges <leltondev@gmail.com>
2022-07-04 20:07:24 -0300

    feat(FormInputValitores): created and implements
    - Create interface:
      - FormValidator
      - FormInputControl
    - Create class
      - FormInputValidator implements FormValidator

---
+++ project_pic/src/app/core/interface/form/validator/form-input-control.ts
@@ -0,0 +1,9 @@
+import { AbstractControl, FormGroup, ValidationErrors } from '@angular/forms';
+
+export interface FormInputControl {
+  getInput(nameInput : string) : AbstractControl | null;
+
+  isTouched(nameInput : string) : boolean | undefined | null;
+
+  isError(nameInput : string, nameError : string) : ValidationErrors | undefined | null;
+}
