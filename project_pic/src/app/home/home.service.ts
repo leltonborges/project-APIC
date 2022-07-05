28fc8b62876ed8df3bb57ee10b955dcabb6f2df6
Lelton Borges <leltondev@gmail.com>
2022-07-05 13:16:48 -0300

    feat(UserNotTakenValidatorService): created

--- project_pic/src/app/home/home.service.ts
+++ project_pic/src/app/home/home.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  constructor(
+    private httpClient : HttpClient
+  ) { }
+
+  checkUserNameTaken(username : string) : Observable<boolean> {
+    return this.httpClient.get<boolean>(`${ environment.baseUrlAPI }/user/exists/${ username }`);
+  }
 }
