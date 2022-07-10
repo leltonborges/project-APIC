8dd8a734e96be1df423edefbba43bd2599a8294b
Lelton Borges <leltondev@gmail.com>
2022-07-09 23:01:40 -0300

    refactory(Routing): config lazy loanding

--- project_pic/src/app/app.module.ts
+++ project_pic/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app.routing.module';
-import { ErrorsModule } from './errors/errors.module';
 import { CommonCustomModule } from './common/common-custom.module';
 
 @NgModule({
