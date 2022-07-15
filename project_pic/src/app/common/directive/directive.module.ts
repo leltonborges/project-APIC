037ab67d9c66fb2b26bbb265f9887f6918d0e3ed
Lelton Borges <leltondev@gmail.com>
2022-07-14 21:54:04 -0300

    feat(BoxHover): Created directive

--- project_pic/src/app/common/directive/directive.module.ts
+++ project_pic/src/app/common/directive/directive.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DarkerOnHoverDirective } from './darker-on-hover/darker-on-hover.directive';
 import { ImmediateClickDirective } from './immediate-click/immediate-click.directive';
+import { BoxHoverDirective } from './box-hover/box-hover.directive';
 
 
 
 @NgModule({
   declarations: [
     DarkerOnHoverDirective,
-    ImmediateClickDirective
+    ImmediateClickDirective,
+    BoxHoverDirective
   ],
     exports: [
         DarkerOnHoverDirective,
